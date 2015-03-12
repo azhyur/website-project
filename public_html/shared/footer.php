b49ea5c91d155fddc16797dfcc37623d939fd455
Chris Guevara <christopher.guevara@gmail.com>
2015-03-12 14:23:09 -0500

    separated header and footer into templates and added case for login button

---
+++ public_html/shared/footer.php
@@ -0,0 +1,13 @@
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Auditory Research Laboratory (ARL)</p>
+		    <p>Department of Communication Disorders and Sciences</p>
+		    <p>Rush University Medical Center</p>
+		    <p>203 Senn, 1653 W. Congress Parkway</p>
+		    <p>Chicago, IL 60612</p>
+                </div>
+            </div>
+        </footer>
