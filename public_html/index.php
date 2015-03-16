e2c00c626b50b68eae714bf851f45da27fb9d48a
Analil <amamuc2@uic.edu>
2015-03-16 00:14:36 -0500

    filling in the place holders
    adding pictures and editing summary

--- public_html/index.php
+++ public_html/index.php
@@ -34,34 +34,35 @@
 
         <!-- First Featurette -->
         <div class="featurette" id="about">
-            <img class="featurette-image img-circle img-responsive pull-right" src="http://placehold.it/500x500">
+            <img class="featurette-image img-circle img-responsive pull-right" src="img/Shafiro.gif">
             <h2 class="featurette-heading">Mission Statement
                 <span class="text-muted">Summary</span>
             </h2>
-            <p class="lead">Professor Valeriy Shafiro at Rush University in the department of Communication Disorders specializes in working with audiology and speech perception. Previous work from Professor Shafiro and team has shown clear benefits of home-based computerized auditory training for users of cochlear implants (implantable electronic devices that stimulate the auditory nerve bypassing normal hearing mechanisms).
-            To provide greater access to these training methods and allow more rigorous testing we would like to implement the testing and training protocols online. </p>
+            <p class="lead">Professor Valeriy Shafiro at Rush University in the department of Communication Disorders specializes in working with audiology and speech perception. 
+                <p class="lead">Previous work from Professor Shafiro and team has shown clear benefits of home-based computerized auditory training for users of cochlear implants (implantable electronic devices that stimulate the auditory nerve bypassing normal hearing mechanisms).</p>
+                <p class="lead">To provide greater access to these training methods and allow more rigorous testing we would like to implement the testing and training protocols online. </p>
+            </p>
         </div>
 
         <hr class="featurette-divider">
 
         <!-- Second Featurette -->
         <div class="featurette" id="services">
-            <img class="featurette-image img-circle img-responsive pull-left" src="http://placehold.it/500x500">
-            <h2 class="featurette-heading">The Second Heading
-                <span class="text-muted">Is Pretty Cool Too.</span>
+            <img class="featurette-image img-circle img-responsive pull-left" src="img/ear.png">
+            <h2 class="featurette-heading">Improve
+                <span class="text-muted">sound perception.</span>
             </h2>
-            <p class="lead">PLACEHOLDER: Here we could explain the process of auditory training testing and supply some pictures.</p>
         </div>
 
         <hr class="featurette-divider">
 
         <!-- Third Featurette -->
         <div class="featurette" id="contact">
-            <img class="featurette-image img-circle img-responsive pull-right" src="http://placehold.it/500x500">
-            <h2 class="featurette-heading">The Third Heading
-                <span class="text-muted">Will Seal the Deal.</span>
+            <img class="featurette-image img-circle img-responsive pull-right" src="img/bulb.gif">
+            <h2 class="featurette-heading">Gain confidence
+                <span class="text-muted">in recognizing sounds.</span>
             </h2>
-            <p class="lead">PLACEHOLDER: If readers have made it this far, then they must be interested. We can use this last spot to explain what to do next in order to participate in auditory training.</p>
+<!--             <p class="lead">PLACEHOLDER: If readers have made it this far, then they must be interested. We can use this last spot to explain what to do next in order to participate in auditory training.</p> -->
         </div>
 
         <hr class="featurette-divider">
