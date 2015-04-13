145d8c4cb04a9dfbde0cd6f80de31c9e2d16e3fb
Chris Guevara <cgueva3@uic.edu>
2015-04-13 08:27:37 -0500

    added training name to start screen

--- public_html/trainings/start.php
+++ public_html/trainings/start.php
@@ -1,5 +1,19 @@
 <?php
 	require '../shared/header-tier1.php';
+    $trainingId = $_GET["t"];
+
+	$con = mysqli_connect("localhost", "root", "", "auditory_training");
+
+	if (mysqli_connect_errno()) {
+                 echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+
+	$result = mysqli_query($con, "SELECT * FROM trainings WHERE id = " . $trainingId);
+
+    $row = mysqli_fetch_array($result);
+    $trainingName = $row['name'];
+	$soundResult = mysqli_query($con, "SELECT * FROM trainings_sounds WHERE training_id = " . $trainingId);
+    $soundCount = mysqli_num_rows($soundResult);
 ?>
 	<body>
 <?php 
@@ -9,8 +23,9 @@
 		<div class="row" style="padding-top: 100px; padding-bottom:100px;">
 			<div class="col-md-6 col-md-offset-3 text-center">
 				<h1>Welcome to your training!</h1>
+                <h2><em><?php echo $trainingName; ?></em></h2>
 				<br>
-				<p>In this training you will be asked to identify a series of random sounds.</p>
+                <p>In this training you will be asked to identify <?php echo $soundCount; ?> sounds.</p>
 				<p>When you hear the sound, press the cooresponding button.</P>
 			</div>
 			<div class="col-md-2 col-md-offset-5 text-center" style="margin-top: 10px;">
