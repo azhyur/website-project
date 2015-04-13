3b689ce12a89c7b127ba0cc6557ae0c73abb694d
Chris Guevara <cgueva3@uic.edu>
2015-04-13 08:00:04 -0500

    list clients on admin page

--- public_html/admin/index.php
+++ public_html/admin/index.php
@@ -18,6 +18,35 @@
 
 
 
+    <div class="container">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Language</th>
+            </tr>
+            </thead>
+            <tbody>
+<?php
+
+            $con = mysqli_connect("localhost", "root", "", "auditory_training");
+
+            if (mysqli_connect_errno()) {
+                echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            }
+
+            $result = mysqli_query($con, "SELECT * FROM users WHERE role = 'client'");
+
+	        while($row = mysqli_fetch_array($result)){
+                echo "<tr><td>" . $row['firstname'] . "</td><td>" . $row['lastname'] . "</td><td>" . $row['email'] . "</td><td>" . $row['language'] . "</td></tr>";
+            }
+
+?>
+            <tbody>
+        </table>
+</div>
 
 
 <?php
