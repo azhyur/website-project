9e79cabf6925c6fe126c0e9d709339d465b9202b
unknown <ecurri3@uic.edu>
2015-04-25 14:30:49 -0500

    questionnaire now enters data into correct user

--- public_html/registration/questionnaire_user.php
+++ public_html/registration/questionnaire_user.php
@@ -27,19 +27,14 @@
 	$a2 = mysqli_real_escape_string($con, $_POST['q2']);
 	$a3 = mysqli_real_escape_string($con, $_POST['q3']);
 	$a4 = mysqli_real_escape_string($con, $_POST['q4']);
+	$userid = mysqli_real_escape_string($con, $_SESSION['sess_user_id']);
 
 	if(strlen($_POST['q1']) < 1 || strlen($_POST['q2']) < 1 || strlen($_POST['q3']) < 1 || strlen($_POST['q4']) < 1)
 		header("location: questionnaire-form.php");
 
-	$result = mysqli_query($con, "SELECT * FROM users");
-
-	while($row = mysqli_fetch_array($result)){
-		if(strtoupper($_SESSION['sess_username']) == strtoupper( $row['username'])){
-			$sql="INSERT INTO users (gender, device, duration, experience) VALUES ('$a1', '$a2', '$a3', '$a4')";
-			if(!mysqli_query($con, $sql)){
-	        	die('Error: ' . mysqli_error($con));
-	        }
-		}
+	$sql="UPDATE auditory_users SET gender='$a1', device='$a2', duration='$a3', experience='$a4' WHERE id='$userid'";
+	if(!mysqli_query($con, $sql)){
+		die('Error: ' . mysqli_error($con));
 	}
 
 	header("location: ../trainings/index.php");
